8945b00919fea02313e1c85c5a7b05fbc9ce077e
= <=>
2024-04-14 21:51:41 +1000

    factorised

--- server.c
+++ server.c
@@ -9,79 +9,80 @@
 #define PORT 8080            // Port number for the server
 #define BUFFER_SIZE 1      // Size of the buffer for sending/receiving data
 
-int main() {
-    int server_fd, new_socket;             // File descriptors for the server socket and new connections
-    struct sockaddr_in address;            // Structure to hold server address information
+struct sockaddr_in get_address_struct() {
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;          // Set address family to IPv4
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);        // Convert port number to network byte order
+    return address;
+}
+
+int get_listening_socket() {
+    int server_fd;            // File descriptors for the server socket and new connections
     int opt = 1;                           // Option value for setsockopt()
-    int addrlen = sizeof(address);         // Length of the address structure
-    char buffer[BUFFER_SIZE] = {0};        // Buffer for sending/receiving data
-    const char *hello = "Hello from server"; // Message to send to the client
-    // Create (listening) socket file descriptor
-    // socket() tells the OS to allocate resources for a socket. The int return value
-    // is the unique address for that socket.
-    // server_fd is a LISTENING socket, it only accepts incoming connections and assigns the
-    // a unique socket.
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");           // Print error message if socket creation fails
         exit(EXIT_FAILURE);                // Exit the program with failure status
     }
-    // Attach socket to port 8080 and enable address reuse
-    // setsockopt sets options for a socket.
-    // server_fd is the socket
-    // SOL_SOCKET means we're setting the option at socket level
-    // SO_REUSEADDR is the option: make local addresses 'resuable'. This is because the OS
-    // blocks a port from other bindings for a short period after a socket is closed. This
-    // prevents conflicts with other processes. It's mostly for rapid-starting your server.
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
         perror("setsockopt");              // Print error message if setsockopt() fails
         exit(EXIT_FAILURE);                // Exit the program with failure status
     }
-    address.sin_family = AF_INET;          // Set address family to IPv4
-    // INADDR_ANY allows connections from any interface, since a machine might have several ways
-    // that it's connected to a network. For a client, this isn't super relevant, as it includes
-    // localhost 127.0.0.1, which is where the client is calling from. This would become relevant
-    // when I want to connect to this server from a different machine. It would then listen on
-    // the laptops network card. The loopback address is hosted locally, and doesnt touch any
-    // network intefaces. It is special network interface that is internal to the OS.
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);        // Convert port number to network byte order
-    // Bind socket to the specified address
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+    return server_fd;
+}
+
+void bind_socket_to_address(struct sockaddr_in address, int server_fd) {
+    int addrlen = sizeof(address);         // Length of the address structure
+    if (bind(server_fd, (struct sockaddr *)&address, addrlen)<0) {
         perror("bind failed");              // Print error message if binding fails
         exit(EXIT_FAILURE);                // Exit the program with failure status
     }
-    // TODO: Idea. For now, understand how client-server works step-by-step
-    // TODO: Quick refresher on structs
+}
+
+int accept_client(struct sockaddr_in address, int server_fd) {
+    printf("Accepting...\n");
+    int new_socket;
+    int addrlen = sizeof(address);         // Length of the address structure
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("accept");                   // Print error message if accept() fails
+        exit(EXIT_FAILURE);                // Exit the program with failure status
+    }
+    return new_socket;
+}
+
+void do_session(int new_socket) {
+    while (true) {
+        char buffer[BUFFER_SIZE] = {0};        // Buffer for sending/receiving data
+        printf("Reading...\n");
+        int bytes_read = read(new_socket, buffer, BUFFER_SIZE);
+        if (bytes_read < 0) {
+            printf("An error ocurred while reading the message. Going back to accepting state.\n");
+            break;
+        }
+        if (bytes_read == 0) {
+            printf("Client disconnected. Going back to accepting state.\n");
+            break;
+        }
+        //TODO: Interesting stuff here. It's all nicely abstracted now
+        printf("Received: %c\n", buffer[0]); 
+        char* response = &buffer[0];
+        printf("Returning: %c\n", ++*response);
+        send(new_socket, response, 1, 0);
+    }
+}
+
+int main() {
+    int server_fd = get_listening_socket();
+    struct sockaddr_in address = get_address_struct();
+    bind_socket_to_address(address, server_fd);
     while (true) {
         printf("Listening...\n");
-        // listen on this socket with a queue of up to 3 connections. When the client calls the
-        // request first goes to this queue.
         if (listen(server_fd, 3) < 0) {
-            perror("listen");                   // Print error message if listen() fails
-            exit(EXIT_FAILURE);                // Exit the program with failure status
+            perror("listen");
+            exit(EXIT_FAILURE);
         }
-        // Block until a new connection is made
-        printf("Accepting...\n");
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-            perror("accept");                   // Print error message if accept() fails
-            exit(EXIT_FAILURE);                // Exit the program with failure status
-        }
-        while (true) {
-            printf("Reading...\n");
-            // Block until more data is sent
-            int bytes_read = read(new_socket, buffer, BUFFER_SIZE);
-            if (bytes_read < 0) {
-                printf("An error ocurred while reading the message!");
-            }
-            if (bytes_read == 0) {
-                printf("Client disconnected. Going back to accepting state.\n");
-                break;
-            }
-            printf("Received: %c\n", buffer[0]); 
-            char* response = &buffer[0];
-            printf("Returning: %c\n", ++*response);
-            send(new_socket, response, 1, 0);
-        }
+        int new_socket = accept_client(address, server_fd);
+        do_session(new_socket);
     }
     printf("Exiting...");
     return 0;
